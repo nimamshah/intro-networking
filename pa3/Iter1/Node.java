677a620b93979b4c02a8a571499338655a94ad74
Nick <nimamshah@ufl.edu>
2017-04-10 20:27:28 -0400

    Finishing PA3

--- pa3/Iter1/Node.java
+++ pa3/Iter1/Node.java
@@ -3,6 +3,7 @@ public class Node {
   private int[] links;
   private static final int inf = 100;
 
+  // Helper class to model Nodes in a network
   public Node(int id, String[] links) {
     this.id = id;
     this.links = new int[links.length];
@@ -12,14 +13,18 @@ public class Node {
     }
   }
 
+  // Returns the ID of the current node
   public int getId() {
     return id;
   }
 
+  // Returns the weight of a specified link
   public int getLinkWeight(int i) {
     return links[i];
   }
 
+  // Returns string representation of node
+  //  this looks like the single line representation
   public String toString() {
     String s = "";
     for (int i = 0; i < links.length; i++) {
--- pa3/Iter1/linkstate.java
+++ pa3/Iter1/linkstate.java
@@ -7,15 +7,17 @@ import java.util.ArrayList;
 
 
 public class linkstate {
-  // List<List<Character>> table = new ArrayList<List<Character>>();
-  // String[][] table;
   Node[] nodes;
-  String nprime;
-  String nprimeIndices;
+  String nprime = "";
+  String nprimeIndices = "";
   Node currentNode;
   int step = 0;
   boolean header = false;
   int size = -1;
+  private static final String dash1 = "--------";
+  private static final String dash2 = dash1 + dash1;
+  String fittedDashes = "";
+  String tab = "";
 
   // Parallel arrays used for tracking cost of least-cost path
   //  and previous node along current least-cost path
@@ -30,13 +32,39 @@ public class linkstate {
     System.out.println(s);
   }
 
+  // Attempts to set number of tabs to align columns as N' grows in length
+  // Does not work perfectly under all cases, but usually within one tab
+  private void setTab() {
+    tab = "";
+    String s = "";
+    int i = -1;
+    for (i = 1; i < size+1; i++) {
+      s += Integer.toString(i) + ",";
+    }
+
+    double q = (s.length() - nprime.length())/8.0;
+
+    if (fittedDashes.equals("")) {
+      for (int j = 0; j < size-1; j++) fittedDashes += dash2;
+      for (int j = 0; j < Math.ceil(q); j++) {
+        fittedDashes += dash1;
+      }
+    }
+
+    for (i = 0; i < (Math.ceil(q)); i++) {
+      tab += "\t";
+    }
+    if (q > 0.5 && q < 1.0) {
+      tab += "\t";
+    }
+  }
+
   // Reads file to form table of node data
   private void initialize(String filename) {
     // JDK7 try-with-resources
     try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
       String currentLine;
 
-      // int row = 0;
       int i = 0;
 
       // Loop to EOF
@@ -55,22 +83,13 @@ public class linkstate {
 
         nodes[i] = new Node(i+1, tokens);
         i++;
-
-        // if (table == null) {
-        //   size = tokens.length;
-        //   table = new String[size][size];
-        // }
-        //
-        // for (int col = 0; col < size; col++) {
-        //   table[row][col] = tokens[col];
-        // }
-        // row++;
       }
 
-      log("------ Nodes ------");
-      for (i = 0; i < nodes.length; i++) {
-        log(nodes[i].toString());
-      }
+      // Uncomment to check that nodes are loaded properly
+      // log("------ Nodes ------");
+      // for (i = 0; i < nodes.length; i++) {
+      //   log(nodes[i].toString());
+      // }
 
       // Set N' = {1}
       currentNode = nodes[0];
@@ -81,42 +100,38 @@ public class linkstate {
         pv[i] = Integer.toString(currentNode.getId());
       }
       printProgress();
-
-      // for (i = 0; i < table.length; i++) {
-      //   for (int j = 0; j < table[i].length; j++) {
-      //     // log(table[i][j]);
-      //     System.out.print(table[i][j]);
-      //   }
-      //   System.out.println();
-      // }
-
-
     } catch (IOException e) {
       e.printStackTrace();
     }
   }
 
+  // Helper function to determine when LS algo has completed
   private boolean done() {
     String[] nprimeNodes = nprime.split(",");
     if (nprimeNodes.length == size) return true;
     else return false;
   }
 
-  private int min(int a, int b) {
-    if (a < b) return a;
-    else return b;
-  }
-
+  // Runs Link-State algorithm
   private void compute() {
+    String[] skipIndices = new String[0];
+    boolean firstIndex = true;
+
     while (!done()) {
       // Find w not in N' such that D(w) is a minimum
       int iMin = -1;
       int min = inf;
+      boolean skip = false;
       for (int i = 0; i < Dv.length; i++) {
-        for (int j = 0; j < nprimeIndices; j++) {
-          if (i == j) continue;
+        for (int j = 0; j < skipIndices.length; j++) {
+          if (i == Integer.parseInt(skipIndices[j])) {
+            skip = true;
+            continue;
+          }
         }
-        if (Dv[i] < min) {
+        if (skip) {
+          skip = false;
+        } else if (Dv[i] < min) {
           min = Dv[i];
           iMin = i;
         }
@@ -124,31 +139,43 @@ public class linkstate {
 
       // Add w to N'
       nprime += "," + Integer.toString(nodes[iMin+1].getId());
-      nprimeIndices += "," + Integer.toString(nodes[iMin+1].getId()-1);
+      if (firstIndex) {
+        nprimeIndices += Integer.toString(nodes[iMin+1].getId()-2);
+        firstIndex = false;
+      }
+      else nprimeIndices += "," + Integer.toString(nodes[iMin+1].getId()-2);
+      skipIndices = nprimeIndices.split(",");
 
-      // log(nprime);
       currentNode = nodes[iMin+1];
 
       // Update D(v) for each neighbor v of w and not in N':
       //  D(v) = min( D(v), D(w) + c(w,v) )
       for (int i = 0; i < Dv.length; i++) {
-        Dv[i] = min(Dv[i], Math.abs(Dv[iMin] + Dv[i]));
+        if (Dv[i] <= Math.abs(Dv[iMin] + nodes[iMin+1].getLinkWeight(i+1))); // do nothing
+        else {
+          Dv[i] = Math.abs(Dv[iMin] + nodes[iMin+1].getLinkWeight(i+1));
+          pv[i] = Integer.toString(nodes[iMin+1].getId());
+        }
       }
       printProgress();
     }
   }
 
+  // Logs the current step
   private void printProgress() {
+    setTab();
     int i = 0;
     if (!header) {
-      String head = "Step\tN'\t";
+      log(fittedDashes);
+      String head = "Step\tN'" + tab;
       for (i = 0; i < Dv.length; i++) {
         head += "D(" + (i+2) + "),p(" + (i+2) + ")\t";
       }
       log(head);
+      log(fittedDashes);
     }
 
-    String s = Integer.toString(step) + "\t" + nprime + "\t";
+    String s = Integer.toString(step) + "\t" + nprime + tab;
     for (i = 0; i < Dv.length; i++) {
       if (Dv[i] == inf) s += "N";
       else s += Integer.toString(Dv[i]) + "," + pv[i];
@@ -157,8 +184,10 @@ public class linkstate {
     header = true;
     step++;
     log(s);
+    log(fittedDashes);
   }
 
+  // Initializes data structure of nodes and begins LS algo
   public static void main(String[] args) {
     linkstate ls = new linkstate();
     ls.initialize(args[0]);
